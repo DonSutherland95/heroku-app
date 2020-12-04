2720716ad16a99fefbdbd8fce0a288cfecc467d4
DonSutherland95 <58916345+DonSutherland95@users.noreply.github.com>
2020-12-04 09:53:45 -0500

    update

---
+++ index.js
@@ -0,0 +1,23 @@
+const dotenv = require('dotenv')
+dotenv.config()
+
+const express = require('express')
+const port = process.env.PORT || 4000
+
+console.log('web 36 rocks')
+console.log(__dirname)
+
+console.log(process.env.LADY)
+console.log(process.env.FOO)
+console.log(process.env.ALWAYS)
+console.log(process.env.PORT)
+
+const app = express()
+
+app.use('/api/*', (_, res) => {
+  res.json({ data: 'web 36 rocks'})
+})
+
+app.listen(port, () => {
+  console.log(`listening on ${port}`)
+})
